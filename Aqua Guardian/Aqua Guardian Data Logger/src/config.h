7078c49f24fe8a23353be6bfd092eb403595dc8c
Anwar Khan <ahselectronicsclubteam@gmail.com>
2026-09-12 21:52:23 -0400

    timing

--- Aqua Guardian/Aqua Guardian Data Logger/src/config.h
+++ Aqua Guardian/Aqua Guardian Data Logger/src/config.h
@@ -13,7 +13,7 @@
 #define DEVICE_ID "AG-01"
 #define DEEP_SLEEP_DURATION_SEC 2700  // Sleep for 45 minutes between measurement/transmission cycles
 #define SENSOR_READ_INTERVAL_MS 1000 // Take one sample every second
-#define MAX_SAMPLES_PER_SESSION 5  // Take 5 samples = 5-second measurement window
+#define MAX_SAMPLES_PER_SESSION 20  // Take 20 samples = 20-second measurement window
 
 
 // --- API Endpoint Configuration ---
